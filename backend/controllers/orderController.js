722d40738b5a65f138b9352096e8e489c1ded3ea
Shubham Shukla <shubshukla2332@gmail.com>
2025-06-27 16:42:43 +0530

    some changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -288,7 +288,7 @@ const placeOrderRazorpay = async (req, res) => {
     await newOrder.save();
 
     const options = {
-      amount: (amount) * 100,
+      amount: Math.round(amount * 100),
       currency: currency.toUpperCase(),
       receipt: newOrder._id.toString(),
     };
